ab0651951628ff1b2df8c306247253e4c0a4d4ba
grm34 <grm34@users.noreply.github.com>
2014-10-07 20:08:47 +0200

    Update gitbot.py

--- gitbot.py
+++ gitbot.py
@@ -25,10 +25,10 @@ kill_bot = ''                                           #--> When screen in use
 hist = os.environ.get("HOME")+"/gitbot/git_hist.entry"
 
 def on_welcome(server, event):
-    if password:
-        server.privmsg("nickserv", "IDENTIFY %s" % password)
-    server.privmsg("chanserv", "SET irc_auto_rejoin ON")
-    server.privmsg("chanserv", "SET irc_join_delay 0")
+    if (password):
+        server.privmsg("nickserv", "IDENTIFY {0}".format(password))
+        server.privmsg("chanserv", "SET irc_auto_rejoin ON")
+        server.privmsg("chanserv", "SET irc_join_delay 0")
     server.join(chan)
 
 def on_kick(server, event):
@@ -50,11 +50,11 @@ def on_privmsg(server, event):
         elif ('!p' == arguments[0]):
             server.part(message[3:])
         else:
-            server.privmsg(author, "n00b "+author+" ! "
-                "Tu causes à ton bot là !")
+            server.privmsg(author, "n00b {0} ! Tu causes à ton bot là !"
+                           .format(author))
     else:
-        server.privmsg(author, "Hey "+author+", t'as craqué ou quoi ? "\
-            "Tu causes à un bot là, spice de n00b !")
+        server.privmsg(author, "Hey {0}, t'as craqué ou quoi ? Tu causes à un"
+                       " bot là, spice de n00b !".format(author))
 
 def on_pubmsg(server, event):
     author = irclib.nm_to_n(event.source())
@@ -81,22 +81,23 @@ server.connect(network, port, nick, ircname=name, ssl=False)
 msgqueue = []
 
 def update_api():
-    git_hist = open(hist, "r")
+    git_hist = open(hist_ankoa, "r")
     filetext = git_hist.read()
     git_hist.close()
     try:
-        req = urllib2.Request(url_api,
-                              headers={"Accept": "application/vnd.github.v3+json"})
-        req.add_header("Authorization", "token %s" % token)
+        req = urllib2.Request(
+            url_api, headers={"Accept": "application/vnd.github.v3+json"})
+        req.add_header("Authorization", "token {0}".format(token))
         data = loads(urllib2.urlopen(req).read())
         for entry in data:
 
             # Global infos
             type = smart_str(entry['type'])
             actor = smart_str(entry['actor']['login'])
-            temp = "["+smart_str(entry['created_at']).replace("-", "/")\
-                                                     .replace("T", "-")\
-                                                     .replace("Z", "")+"]"
+            temp = "[{0}]".format(smart_str(
+                entry['created_at']).replace("-", "/")
+                                    .replace("T", "-")
+                                    .replace("Z", ""))
             hour_utc = temp.split("-")[1].split(":")[0]
             if (hour_utc == "22"):
                 hour_fr = "00"
@@ -106,7 +107,7 @@ def update_api():
                 hour_fr = int(hour_utc)+2
                 hours = "2 3 4 5 6 7 8 9"
                 if str(hour_fr) in hours:
-                    hour_fr = "0%s" % hour_fr
+                    hour_fr = "0{0}".format(hour_fr)
             date = temp.replace(hour_utc, str(hour_fr))
 
             # Push Events
@@ -115,41 +116,50 @@ def update_api():
                 before = smart_str(entry['payload']['before'])[0:10]
                 head = smart_str(entry['payload']['head'])[0:10]
                 compare = "https://github.com/grm34/AnkoA/"\
-                          "compare/%s...%s" % (before, head)
+                          "compare/{0}...{1}".format(before, head)
                 for item in entry['payload']['commits']:
-                    action = smart_str(item['message']).replace('\n\n', ' - ')
-                    id = "[Push Event] "+actor+" pushed to "+branch+\
-                         ": "+action+" -> "+compare+" "+date
-                    if (id not in filetext):
-                        git_hist = open(hist, "a")
-                        git_hist.write(id + "\n")
+                    action = smart_str(item['message']).replace('\n\n', ' - ')\
+                                                       .replace('\n', ' - ')
+                    msg = "[Push Event] {0} pushed to {1}: {2} -> {3} {4}"\
+                          .format(actor, branch, action, compare, date)
+                    if (msg not in filetext):
+                        git_hist = open(hist_ankoa, "a")
+                        git_hist.write("{0}\n".format(msg))
                         git_hist.close()
-                        msgqueue.append(id)
+                        msgqueue.append(msg)
 
             # Gollum Events
             if (type == "GollumEvent"):
                 for item in entry['payload']['pages']:
-                    id = "[Wiki Event] "+actor+" "+smart_str(item['action'])+\
-                         " "+smart_str(item['title'])+" -> "+\
-                         smart_str(item['html_url'])+" "+date
-                    if (id not in filetext):
-                        git_hist = open(hist, "a")
-                        git_hist.write(id + "\n")
+                    verif = "[Wiki Event] {0} {1} {2} -> {3} {4}"\
+                            .format(actor, smart_str(item['action']),
+                                    smart_str(item['title']),
+                                    smart_str(item['html_url']),
+                                    date.split('-')[0])
+                    if (verif not in filetext):
+                        msg = "[Wiki Event] {0} {1} {2} -> {3} {4}"\
+                              .format(actor, smart_str(item['action']),
+                                      smart_str(item['title']),
+                                      smart_str(item['html_url']), date)
+                        git_hist = open(hist_ankoa, "a")
+                        git_hist.write("{0}\n".format(msg))
                         git_hist.close()
-                        msgqueue.append(id)
+                        msgqueue.append(msg)
 
             # Pull Request Events
             if (type == "PullRequestEvent"):
-                pull_title = smart_str(entry['payload']['pull_request']['title'])
+                pull_title = smart_str(
+                    entry['payload']['pull_request']['title'])
                 pull_action = smart_str(entry['payload']['action'])
-                pull_url = smart_str(entry['payload']['pull_request']['html_url'])
-                id = "[Request Event] "+actor+" "+pull_action+" request: "+\
-                     pull_title+" -> "+pull_url+" "+date
-                if (id not in filetext):
-                    git_hist = open(hist, "a")
-                    git_hist.write(id + "\n")
+                pull_url = smart_str(
+                    entry['payload']['pull_request']['html_url'])
+                msg = "[Request Event] {0} {1} request: {2} -> {3} {4}"\
+                      .format(actor, pull_action, pull_title, pull_url, date)
+                if (msg not in filetext):
+                    git_hist = open(hist_ankoa, "a")
+                    git_hist.write("{0}\n".format(msg))
                     git_hist.close()
-                    msgqueue.append(id)
+                    msgqueue.append(msg)
 
             # Issues Events
             if (type == "IssuesEvent" or type == "IssueCommentEvent"):
@@ -158,16 +168,17 @@ def update_api():
                 issue_url = smart_str(entry['payload']['issue']['html_url'])
                 if (type == "IssueCommentEvent" and issue_action == "created"):
                     issue_action = "commented"
-                id = "[Issue Event] "+actor+" "+issue_action+" issue: "+\
-                     issue_title+" -> "+issue_url+" "+date
-                if (id not in filetext):
-                    git_hist = open(hist, "a")
-                    git_hist.write(id + "\n")
+                msg = "[Issue Event] {0} {1} issue: {2} -> {3} {4}"\
+                      .format(actor, issue_action,
+                              issue_title, issue_url, date)
+                if (msg not in filetext):
+                    git_hist = open(hist_ankoa, "a")
+                    git_hist.write("{0}\n".format(msg))
                     git_hist.close()
-                    msgqueue.append(id)
+                    msgqueue.append(msg)
 
-    except (HTTPError, KeyError, URLError) as e:
-        server.notice(bot_owner, "[API ERROR] "+str(e))
+    except (HTTPError, KeyError, URLError, IOError, ValueError) as e:
+        server.notice(bot_owner, "[API ERROR] {0}".format(str(e)))
         pass
 
     threading.Timer(30.0, update_api).start()
